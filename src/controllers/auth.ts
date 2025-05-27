26d4425a3b4702be9d9308906441a5edb82aa742
Wiktor Tarnowski <MrBlack1223@wp.pl>
2025-05-27 21:50:11 +0200

    feat(auth): implement JWT access and refresh token logic in login and refreshToken endpoints, set tokens as HttpOnly cookies

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -27,20 +27,68 @@ export class AuthController {
                 res.status(statusCode.INTERNAL_SERVER_ERROR).json({ message: "JWT secret not configured" });
                 return;
             }
-            const token = jwt.sign({ id: user._id }, jwtSecret, { expiresIn: '1h' });
-
-            res.status(statusCode.SUCCESS).cookie('token', token, {
+            const token = jwt.sign({ id: user._id }, jwtSecret, { expiresIn: '15m' });
+            const refreshToken = jwt.sign({ id: user._id }, jwtSecret, { expiresIn: '1d' });
+            res
+            .status(statusCode.SUCCESS)
+            .cookie('token', token, {
                 httpOnly: true,
                 sameSite: 'lax',
-                maxAge: 3600000,
+                maxAge: 900000,
                 secure: process.env.NODE_ENV === 'production',
-            }).json(
+            })
+            .cookie('refreshToken', refreshToken, {
+                httpOnly: true,
+                sameSite: 'lax',
+                maxAge: 86400000,
+                secure: process.env.NODE_ENV === 'production'})
+            .json(
             { message: "Login successful", user: { _id: user._id, name: user.name, email: user.email }}
         )
             return;
             
         } catch (error) {
             console.error("Error logging in:", error);
+            res.status(statusCode.INTERNAL_SERVER_ERROR).json({ message: "Internal server error" });
+        }
+    }
+    public async refreshToken(req: Request, res: Response): Promise<void> {
+        const token = req.cookies.refreshToken;
+        if (!token) {
+            res.status(statusCode.UNAUTHORIZED).json({ message: "No refresh token provided" });
+            return;
+        }
+        try {
+            const jwtSecret = process.env.JWT_SECRET;
+            if (!jwtSecret) {
+                res.status(statusCode.INTERNAL_SERVER_ERROR).json({ message: "JWT secrett not configured" });
+                return;
+            }
+            jwt.verify(token, jwtSecret, (err: any, decoded: any) => {
+                    if (err) {
+                        return res.status(statusCode.FORBIDDEN).json({ message: 'Failed to authenticate token' });
+                    }
+
+                    const newToken = jwt.sign({ id: decoded.id }, jwtSecret, { expiresIn: '15m' });
+                    const newRefreshToken = jwt.sign({ id: decoded.id }, jwtSecret, { expiresIn: '1d' });
+                    
+                    res
+                    .status(statusCode.SUCCESS)
+                    .cookie('token', newToken, {
+                        httpOnly: true,
+                        sameSite: 'lax',
+                        maxAge: 900000,
+                        secure: process.env.NODE_ENV === 'production',
+                    })
+                    .cookie('refreshToken', newRefreshToken, {
+                        httpOnly: true,
+                        sameSite: 'lax',
+                        maxAge: 86400000,
+                        secure: process.env.NODE_ENV === 'production',
+                    })
+                });
+        } catch (error) {
+            console.error("Error refreshing token:", error);
             res.status(statusCode.INTERNAL_SERVER_ERROR).json({ message: "Internal server error" });
         }
     }
